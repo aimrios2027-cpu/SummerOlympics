bf6f6bb151bc83ea6a1659c19e3f33962af1b9c1
Aimee Rios <aimrios2027@gmail.com>
2026-04-17 12:15:10 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by RIOS, AIMEE on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
